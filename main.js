0f5c8e806c68a1b9f1685408bf527f8021bdb97f
franckuestein <franckuestein@users.noreply.github.com>
2016-10-02 22:39:34 +0200

    Quit problem solved
    Completely remove the bitcoinbar app after pressing the “Quit” button.

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
+const electron = require('electron');
+const app = electron.app;
 var menubar = require('menubar');
 var mb = menubar({width: 400, height: 400, fullscreen: false})
 
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+    app.quit();
+});
+
 mb.on('ready', function ready () {
 	console.log('Running bitcoinbar ticker in your operating system.')
-})
+});
 
 /*mb.on('after-create-window', function(){
 	mb.window.openDevTools();
